27deb263e0e8b7263f66d4acbd728e2bed65e6d6
Daniel Duffy <dpd1208@gmail.com>
2019-03-10 21:10:21 -0600

    Updates

---
+++ precache-manifest.7ed3bfb41af8d5060d7621cc8b1d46cc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "837dbec948331a4448d0f02b0da1ebbd",
+    "url": "/ClickyGame/static/media/gemini.837dbec9.png"
+  },
+  {
+    "revision": "ac2120bfdee3690b84c3",
+    "url": "/ClickyGame/static/css/main.3badc0ed.chunk.css"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "99362a6a19b923c7f0ae",
+    "url": "/ClickyGame/static/js/2.5703650f.chunk.js"
+  },
+  {
+    "revision": "8bff9b14945ec267299740a4e5f95a44",
+    "url": "/ClickyGame/static/media/aquarius.8bff9b14.png"
+  },
+  {
+    "revision": "d5aac066384f63c8a2d6cc9be5cb3c1d",
+    "url": "/ClickyGame/static/media/aries.d5aac066.png"
+  },
+  {
+    "revision": "bf6497e5b682d641581f1a6a97cac039",
+    "url": "/ClickyGame/static/media/cancer.bf6497e5.png"
+  },
+  {
+    "revision": "5f9c9924747bedd46bc2bd253915a8d7",
+    "url": "/ClickyGame/static/media/capricorn.5f9c9924.png"
+  },
+  {
+    "revision": "ac2120bfdee3690b84c3",
+    "url": "/ClickyGame/static/js/main.3ad9036e.chunk.js"
+  },
+  {
+    "revision": "9e111645845941cbf3af16503de85449",
+    "url": "/ClickyGame/static/media/leo.9e111645.png"
+  },
+  {
+    "revision": "1cf74419b480cf2bf9ef12e1a706f73c",
+    "url": "/ClickyGame/static/media/libra.1cf74419.png"
+  },
+  {
+    "revision": "1d377df98e00319a90d61da8186ca660",
+    "url": "/ClickyGame/static/media/pisces.1d377df9.png"
+  },
+  {
+    "revision": "3cea56aef2923b2774c7d357620dfaa5",
+    "url": "/ClickyGame/static/media/sagittarius.3cea56ae.png"
+  },
+  {
+    "revision": "be127ddc2114985228f24e7cac9be234",
+    "url": "/ClickyGame/static/media/scorpio.be127ddc.png"
+  },
+  {
+    "revision": "cba86121813932143907c3784b6139e4",
+    "url": "/ClickyGame/static/media/taurus.cba86121.png"
+  },
+  {
+    "revision": "ac78bd7d252448bd32108c8e059c5006",
+    "url": "/ClickyGame/static/media/virgo.ac78bd7d.png"
+  },
+  {
+    "revision": "4961537904160b993fe4e81e4d9f3f26",
+    "url": "/ClickyGame/index.html"
+  }
+];
